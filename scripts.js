1698b2b179ef5a4b7588f0bd77e51e1987888b3e
Vitor dos Santos Silva Lôbo <vitorlobo@aluno.ufrb.edu.br>
2023-11-21 20:20:27 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -4,3 +4,27 @@ const botaoIgual = document.querySelector(".igual");
 const botaoPonto = document.querySelector(".ponto");
 const botoesNumeros = document.querySelector(".num");
 const botoesOperadores = document.querySelector(".operador")
+
+//Variaveis
+let operacaoAtual = "";
+let operador = "null";
+let valorAnterior = "";
+let calculando = false;
+
+//Funções
+const atualizaDisplay = ()=>{
+    display.value = operacaoAtual;
+};
+
+const insereNumero = (evento) => {
+    if (calculando){
+        operacaoAtual = evento.target.textContet;
+        calculando = false;
+    } else {
+        operacaoAtual += evento.target.textContet;
+    }
+    atualizaDisplay();
+}
+
+//EventListeners
+botoesNumeros.forEach((botao) => botao.addEventListener("click", insereNumero));
