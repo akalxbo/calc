bca80acc77bec9a3de1f89698d5385dceac2653f
Vitor dos Santos Silva Lôbo <vitorlobo@aluno.ufrb.edu.br>
2023-11-21 20:00:37 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+//Selecionando dos elementos
+const display = document.querySelector("#displayInput");
+const botaoIgual = document.querySelector(".igual");
+const botaoPonto = document.querySelector(".ponto");
+const botoesNumeros = document.querySelector(".num");
+const botoesOperadores = document.querySelector(".operador")
